103a20051c50c896b4f55ee68eaeceb267f34f85
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-09-04 13:28:24 +0200

    Added AnalyticsService.

---
+++ lib/modules/analytics/src/services/analytics_service.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:flutter/widgets.dart';
+import '../models/base_analytics_event.dart';
+
+abstract class AnalyticsService {
+  static final _analytics = FirebaseAnalytics();
+
+  static final _observer = FirebaseAnalyticsObserver(analytics: _analytics);
+
+  static NavigatorObserver get observer => _observer;
+
+  static Future<void> trackEvent(BaseAnalyticsEvent event) async =>
+      await _analytics.logEvent(name: event.name, parameters: event.parameters);
+}
