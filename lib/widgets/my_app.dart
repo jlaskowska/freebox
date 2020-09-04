d32a01750a73d21b77a1bcdde6c1e3c2ed9a074e
Joanna Laskowska <58981559+jlaskowska@users.noreply.github.com>
2020-09-04 13:35:02 +0200

    Feature/firebase analytics (#5)
    Added analytics service which uses Firebase.
    
    Commits:
    * Added firebase_analytics as dependency.
    * Added BaseAnalyticsEvent.
    * Added AnalyticsService.
    * Added analytics library.
    * Enabled firebase debug for dev mode for iOS.
    * Added AnalyticsService to MyApp.

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:freebox/configs/app_config.dart';
+import 'package:freebox/modules/analytics/analytics.dart';
 import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Freebox'),
-        ),
-        body: Center(
-          child: Text(
-            context.watch<AppConfig>().buildEnvironment.toString(),
-          ),
+      navigatorObservers: [
+        AnalyticsService.observer,
+      ],
+      home: _HomeScreen(),
+    );
+  }
+}
+
+class _HomeScreen extends StatelessWidget {
+  const _HomeScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Freebox'),
+      ),
+      body: Center(
+        child: Text(
+          context.watch<AppConfig>().buildEnvironment.toString(),
         ),
       ),
     );
