813b809ad137bbe38c6338626a68e4ef0a509bb1
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-09-05 14:40:50 +0200

    Updated AppColors.

---
+++ lib/configs/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const navyBlue = Color(0xff010b92);
+  static const rosa = Color(0xfff576aa);
+  static const white = Colors.white;
+}
