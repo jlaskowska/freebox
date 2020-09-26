4aefde78b36b02d4a2201de52faccff3951bf832
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-09-26 17:26:15 +0200

    Created MainScreen.

---
+++ lib/widgets/main_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:freebox/configs/app_colors.dart';
+import 'package:freebox/widgets/freebox_screen.dart';
+import 'package:freebox/widgets/map_screen.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int selectedIndex = 0;
+  static const List<Widget> screens = <Widget>[
+    FreeboxScreen(),
+    MapScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) => setState(() {
+          selectedIndex = index;
+        }),
+        showSelectedLabels: true,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              MdiIcons.packageVariantClosed,
+              color: AppColors.blue,
+            ),
+            title: Text(
+              'Freeboxes',
+              style: TextStyle(
+                color: AppColors.blue,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              MdiIcons.mapSearchOutline,
+              color: AppColors.blue,
+            ),
+            title: Text(
+              'Map',
+              style: TextStyle(
+                color: AppColors.blue,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: screens[selectedIndex],
+      ),
+    );
+  }
+}
