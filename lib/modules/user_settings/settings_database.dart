bdc3a8910de7c30fed6afdcb1282945cb151ba83
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-05 17:26:23 +0200

    Created SettingsDatabase.

---
+++ lib/modules/user_settings/settings_database.dart
@@ -0,0 +1,27 @@
+import 'package:hive/hive.dart';
+
+import 'i_settingsdatabase.dart';
+
+class SettingsDatabase extends ISettingsDatabase {
+  static const _boxName = 'settings';
+
+  Box<dynamic> _box;
+
+  @override
+  Future<void> init() async {
+    _box = await Hive.openBox(_boxName);
+  }
+
+  @override
+  set userSeenOnboarding(value) => _box.put(_Keys.userSeenOnboarding, value);
+  @override
+  bool get userSeenOnboarding => _box.get(_Keys.userSeenOnboarding, defaultValue: _Defaults.userSeenOnboarding);
+}
+
+class _Keys {
+  static const userSeenOnboarding = 'userSeenOnboarding';
+}
+
+class _Defaults {
+  static const userSeenOnboarding = false;
+}
