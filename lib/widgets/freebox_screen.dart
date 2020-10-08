16cb15c80a0194216a0841a489f67c5d8deba57b
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-08 13:47:29 +0200

    Code cleanup.

--- lib/widgets/freebox_screen.dart
+++ lib/widgets/freebox_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freebox/modules/backend_service/bacend_service.dart';
 import 'package:freebox/modules/backend_service/src/models/freebox.dart';
-import 'package:freebox/widgets/custom_circular_progress_indicator.dart';
 import 'package:provider/provider.dart';
 
 class FreeboxScreen extends StatelessWidget {
@@ -9,12 +8,6 @@ class FreeboxScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // print(context.watch<IBackendService>().freeboxStream().toString());
-    final freebox = context.watch<IBackendService>().freeboxStream().listen(
-          (event) => event.map(
-            (e) => Freebox(image: e.image, description: e.description, address: e.address),
-          ),
-        );
     return StreamBuilder(
         stream: context.watch<IBackendService>().freeboxStream(),
         builder: (context, AsyncSnapshot<List<Freebox>> snapshot) {
