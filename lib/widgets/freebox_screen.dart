1771e2c54ea6a06d9a4334f9b6bcc28f3b2eddc1
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-08 13:46:58 +0200

    Updated FreeboxScreen.

--- lib/widgets/freebox_screen.dart
+++ lib/widgets/freebox_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:freebox/modules/backend_service/bacend_service.dart';
+import 'package:freebox/modules/backend_service/src/models/freebox.dart';
+import 'package:freebox/widgets/custom_circular_progress_indicator.dart';
+import 'package:provider/provider.dart';
 
 class FreeboxScreen extends StatelessWidget {
   const FreeboxScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Freebox Screen'),
-    );
+    // print(context.watch<IBackendService>().freeboxStream().toString());
+    final freebox = context.watch<IBackendService>().freeboxStream().listen(
+          (event) => event.map(
+            (e) => Freebox(image: e.image, description: e.description, address: e.address),
+          ),
+        );
+    return StreamBuilder(
+        stream: context.watch<IBackendService>().freeboxStream(),
+        builder: (context, AsyncSnapshot<List<Freebox>> snapshot) {
+          print(snapshot.connectionState);
+          if (snapshot.data != null) {
+            return ListView.builder(
+              itemBuilder: (context, index) => ListTile(
+                title: Text(snapshot.data[index].description),
+              ),
+              itemCount: snapshot.data.length,
+            );
+          } else {
+            return Container();
+          }
+        });
   }
 }
