75a20406210ddc680086b84b5d18b029587c0b28
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-08 16:00:58 +0200

    Updated IBackendService.

--- lib/modules/backend_service/src/services/i_backend_service.dart
+++ lib/modules/backend_service/src/services/i_backend_service.dart
@@ -1,7 +1,17 @@
+import 'dart:io';
+
+import 'package:meta/meta.dart';
+
 import '../models/freebox.dart';
 
 abstract class IBackendService {
   Stream<List<Freebox>> freeboxStream();
 
   Future<void> initializeBackend();
+
+  Future<bool> publishFreebox({
+    @required File image,
+    @required String description,
+    @required String address,
+  });
 }
