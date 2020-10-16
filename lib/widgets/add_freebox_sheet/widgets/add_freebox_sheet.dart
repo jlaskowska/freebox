87437503a548fc35baf3c56d0e58c98b817e7779
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-16 13:16:44 +0200

    Updated AddFreeboxSheet.

--- lib/widgets/add_freebox_sheet/widgets/add_freebox_sheet.dart
+++ lib/widgets/add_freebox_sheet/widgets/add_freebox_sheet.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freebox/configs/app_colors.dart';
 import 'package:freebox/localizations.dart';
-import 'package:freebox/widgets/add_freebox_sheet/cubit/freebox_image_picker_cubit.dart';
+import 'package:freebox/widgets/add_freebox_sheet/cubit/image_picker_cubit.dart';
+import 'package:freebox/widgets/add_freebox_sheet/cubit/image_picker_repository.dart';
+import 'package:freebox/widgets/add_freebox_sheet/cubit/image_picker_respository_impl.dart';
 import 'package:freebox/widgets/custom_circular_progress_indicator.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
@@ -14,78 +16,108 @@ class AddFreeboxSheet extends StatefulWidget {
 }
 
 class _AddFreeboxSheetState extends State<AddFreeboxSheet> {
+  final _descriptionController = TextEditingController();
+  final _adressController = TextEditingController();
+  FocusNode _focusNode1;
+  FocusNode _focusNode2;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _focusNode1 = FocusNode();
+    _focusNode2 = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _focusNode1.dispose();
+    _focusNode2.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _cubit = FreeboxImagePickerCubit();
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: BlocBuilder<FreeboxImagePickerCubit, FreeboxImagePickerState>(
-            cubit: _cubit,
-            builder: (context, state) {
-              if (state is FreeboxImagePickerInitial) {
-                return FreeboxImagePicker(
-                  onPressed: () async => await _cubit.getImage(),
-                  opacity: 0.4,
-                  isImageLoaded: false,
-                  child: Icon(
-                    MdiIcons.cloudUploadOutline,
-                  ),
-                );
-              } else if (state is FreeboxImagePickerLoaded) {
-                return FreeboxImagePicker(
-                  onPressed: () async => await _cubit.getImage(),
-                  isImageLoaded: true,
-                  opacity: 1,
-                  child: Image.file(
-                    _cubit.image,
-                    fit: BoxFit.cover,
-                  ),
-                );
-              } else {
-                return CustomCircularProgressIndicator();
-              }
-            },
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: AppLocalizations.addFreeboxSheetUpTextFieldDescription,
+    return RepositoryProvider<ImagePickerRepository>(
+      create: (context) => ImagePickerRepositoryImpl(),
+      child: BlocProvider<ImagePickerCubit>(
+        create: (context) => ImagePickerCubit(context.repository<ImagePickerRepository>()),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: BlocBuilder<ImagePickerCubit, ImagePickerState>(
+                builder: (context, state) {
+                  if (state is ImagePickerInitial) {
+                    return FreeboxImagePicker(
+                      onPressed: () async => await context.bloc<ImagePickerCubit>().getImage(),
+                      opacity: 0.4,
+                      isImageLoaded: false,
+                      child: Icon(
+                        MdiIcons.cloudUploadOutline,
+                      ),
+                    );
+                  } else if (state is ImagePickerLoaded) {
+                    return FreeboxImagePicker(
+                      onPressed: () async => await context.bloc<ImagePickerCubit>().getImage(),
+                      isImageLoaded: true,
+                      opacity: 1,
+                      child: Image.file(
+                        state.image,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  } else {
+                    return CustomCircularProgressIndicator();
+                  }
+                },
+              ),
             ),
-            maxLines: 2,
-            keyboardType: TextInputType.text,
-            cursorColor: Theme.of(context).primaryColor,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: TextField(
-            decoration: InputDecoration(labelText: AppLocalizations.addFreeboxSheetUpTextFieldAdress),
-            keyboardType: TextInputType.streetAddress,
-            cursorColor: Theme.of(context).primaryColor,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 16,
-          ),
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              AppLocalizations.addFreeboxSheetUploadFreeboxButtonLabel,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                focusNode: _focusNode1,
+                onSubmitted: (String value) => FocusScope.of(context).requestFocus(_focusNode2),
+                controller: _descriptionController,
+                decoration: InputDecoration(
+                  labelText: AppLocalizations.addFreeboxSheetUpTextFieldDescription,
+                ),
+                maxLines: 2,
+                keyboardType: TextInputType.text,
+                cursorColor: Theme.of(context).primaryColor,
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                focusNode: _focusNode2,
+                controller: _adressController,
+                decoration: InputDecoration(labelText: AppLocalizations.addFreeboxSheetUpTextFieldAdress),
+                keyboardType: TextInputType.streetAddress,
+                cursorColor: Theme.of(context).primaryColor,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 16,
+              ),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  AppLocalizations.addFreeboxSheetUploadFreeboxButtonLabel,
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
 
-class FreeboxImagePicker extends StatefulWidget {
+class FreeboxImagePicker extends StatelessWidget {
   final double opacity;
   final Widget child;
   final bool isImageLoaded;
@@ -99,32 +131,27 @@ class FreeboxImagePicker extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _FreeboxImagePickerState createState() => _FreeboxImagePickerState();
-}
-
-class _FreeboxImagePickerState extends State<FreeboxImagePicker> {
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Opacity(
-          opacity: widget.opacity,
+          opacity: opacity,
           child: Padding(
             padding: const EdgeInsets.symmetric(
               horizontal: 8.0,
               vertical: 8,
             ),
             child: Container(
-              color: widget.isImageLoaded ? AppColors.white : Theme.of(context).primaryColor,
+              color: isImageLoaded ? AppColors.white : Theme.of(context).primaryColor,
               height: 150,
               width: 150,
-              child: widget.child,
+              child: child,
             ),
           ),
         ),
         TextButton.icon(
-          onPressed: () async => await widget.onPressed(),
+          onPressed: () async => await onPressed(),
           icon: Icon(MdiIcons.cameraEnhanceOutline),
           label: Text(AppLocalizations.addFreeboxSheetUploadImageButtonLabel),
         )
